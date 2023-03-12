2d6480d37aa0909f128bf785845d0f974e4db9f6
ddy <58058861+ddy-ddy@users.noreply.github.com>
2023-03-12 22:31:52 +0800

    feat: code for frontend

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -1,18 +1,45 @@
 /*
  * @Author: ddy 58058861+ddy-ddy@users.noreply.github.com
  * @Date: 2023-03-07 22:23:47
- * @LastEditTime: 2023-03-07 22:29:40
+ * @LastEditTime: 2023-03-12 22:12:47
  * @Github: https://github.com/ddy-ddy
  * @Website: https://ddy-ddy.com
  */
 import { error } from '@sveltejs/kit';
 
+const data = {
+    id: "A",
+    children: [
+        {
+            id: "A1",
+            children: [
+                { id: "A11" },
+                { id: "A12" },
+                { id: "A13" },
+                { id: "A14" },
+            ],
+        },
+        {
+            id: "A2",
+            children: [
+                {
+                    id: "A21",
+                    children: [{ id: "A211" }, { id: "A212" }],
+                },
+                {
+                    id: "A22",
+                },
+            ],
+        },
+    ],
+};
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
-    const response = await fetch('https://gw.alipayobjects.com/os/basement_prod/6cae02ab-4c29-44b2-b1fd-4005688febcb.json');
-    const data = await response.json();
-    if (response) {
-        return data;
-    }
-    throw error(404, 'Not found');
+    // const response = await fetch('https://gw.alipayobjects.com/os/basement_prod/6cae02ab-4c29-44b2-b1fd-4005688febcb.json');
+    // const data = await response.json();
+    // if (response) {
+    //     return data;
+    // }
+    // throw error(404, 'Not found');
+    return data;
 }
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,102 +1,200 @@
 <script>
   import G6 from "@antv/g6";
   import { onMount } from "svelte";
+  import insertCss from "insert-css";
 
+  insertCss(`
+  .g6-component-toolbar li {
+    list-style-type: none !important;
+  }
+`);
   export let data;
+
   onMount(() => {
-    const container = document.getElementById("graph-container");
-    // 实例化 grid 插件
-    const graph = new G6.Graph({
-      container,
-      width: 800,
-      height: 600,
-      // 节点默认配置
-      defaultNode: {
-        labelCfg: {
-          style: {
+    G6.registerNode("card-node", {
+      draw: function drawShape(cfg, group) {
+        const r = 2;
+        const color = "#5B8FF9";
+        const w = cfg.size[0];
+        const h = cfg.size[1];
+        const shape = group.addShape("rect", {
+          attrs: {
+            x: -w / 2,
+            y: -h / 2,
+            width: w, //200,
+            height: h, // 60
+            stroke: color,
+            radius: r,
             fill: "#fff",
           },
-        },
+          // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
+          name: "main-box",
+          draggable: true,
+        });
+
+        group.addShape("rect", {
+          attrs: {
+            x: -w / 2,
+            y: -h / 2,
+            width: w, //200,
+            height: h / 2, // 60
+            fill: color,
+            radius: [r, r, 0, 0],
+          },
+          // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
+          name: "title-box",
+          draggable: true,
+        });
+
+        // title text
+        group.addShape("text", {
+          attrs: {
+            textBaseline: "top",
+            x: -w / 2 + 8,
+            y: -h / 2 + 2,
+            lineHeight: 20,
+            text: cfg.id,
+            fill: "#fff",
+          },
+          // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
+          name: "title",
+        });
+        cfg.children &&
+          group.addShape("marker", {
+            attrs: {
+              x: w / 2,
+              y: 0,
+              r: 6,
+              cursor: "pointer",
+              symbol: cfg.collapsed ? G6.Marker.expand : G6.Marker.collapse,
+              stroke: "#666",
+              lineWidth: 1,
+              fill: "#fff",
+            },
+            // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
+            name: "collapse-icon",
+          });
+        group.addShape("text", {
+          attrs: {
+            textBaseline: "top",
+            x: -w / 2 + 8,
+            y: -h / 2 + 24,
+            lineHeight: 20,
+            text: "description",
+            fill: "rgba(0,0,0, 1)",
+          },
+          // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
+          name: `description`,
+        });
+        return shape;
       },
-      // 边默认配置
-      defaultEdge: {
-        labelCfg: {
-          autoRotate: true,
-        },
+      setState(name, value, item) {
+        if (name === "collapsed") {
+          const marker = item
+            .get("group")
+            .find((ele) => ele.get("name") === "collapse-icon");
+          const icon = value ? G6.Marker.expand : G6.Marker.collapse;
+          marker.attr("symbol", icon);
+        }
       },
-      // 节点在各状态下的样式
-      nodeStateStyles: {
-        // hover 状态为 true 时的样式
-        hover: {
-          fill: "lightsteelblue",
-        },
-        // click 状态为 true 时的样式
-        click: {
-          stroke: "#000",
-          lineWidth: 3,
-        },
-      },
-      // 边在各状态下的样式
-      edgeStateStyles: {
-        // click 状态为 true 时的样式
-        click: {
-          stroke: "steelblue",
-        },
-      },
-      // 布局
-      layout: {
-        type: "force",
-        linkDistance: 100,
-        preventOverlap: true,
-        nodeStrength: -30,
-        edgeStrength: 0.1,
-      },
-      // 内置交互
+    });
+    const container = document.getElementById("graph-container");
+    const width = container.scrollWidth;
+    const height = container.scrollHeight || 500;
+    const toolbar = new G6.ToolBar({
+      position: { x: 50, y: 100 },
+    });
+    const graph = new G6.TreeGraph({
+      container,
+      width,
+      height,
+      plugins: [toolbar],
+      linkCenter: true,
+      enabledStack: true,
       modes: {
-        default: ["drag-canvas", "zoom-canvas", "drag-node"],
+        default: ["drag-canvas"],
       },
-      animate: true, // Boolean，可选，全局变化时否使用动画过渡
+      defaultNode: {
+        type: "card-node",
+        size: [100, 40],
+      },
+      defaultEdge: {
+        type: "cubic-horizontal",
+        style: {
+          endArrow: true,
+        },
+      },
+      layout: {
+        type: "indented",
+        direction: "LR",
+        dropCap: false,
+        indent: 200,
+        getHeight: () => {
+          return 60;
+        },
+      },
+    });
 
-    });
-    graph.data(data); // 读取数据源到图上
-    graph.render(); // 渲染图
-
-    // 监听鼠标进入节点
-    graph.on("node:mouseenter", (e) => {
-      const nodeItem = e.item;
-      // 设置目标节点的 hover 状态 为 true
-      graph.setItemState(nodeItem, "hover", true);
-    });
-    // 监听鼠标离开节点
-    graph.on("node:mouseleave", (e) => {
-      const nodeItem = e.item;
-      // 设置目标节点的 hover 状态 false
-      graph.setItemState(nodeItem, "hover", false);
-    });
-    // 监听鼠标点击节点
+    graph.data(data);
+    graph.render();
+    graph.fitView();
     graph.on("node:click", (e) => {
-      // 先将所有当前有 click 状态的节点的 click 状态置为 false
-      const clickNodes = graph.findAllByState("node", "click");
-      clickNodes.forEach((cn) => {
-        graph.setItemState(cn, "click", false);
-      });
-      const nodeItem = e.item;
-      // 设置目标节点的 click 状态 为 true
-      graph.setItemState(nodeItem, "click", true);
-    });
-    // 监听鼠标点击节点
-    graph.on("edge:click", (e) => {
-      // 先将所有当前有 click 状态的边的 click 状态置为 false
-      const clickEdges = graph.findAllByState("edge", "click");
-      clickEdges.forEach((ce) => {
-        graph.setItemState(ce, "click", false);
-      });
-      const edgeItem = e.item;
-      // 设置目标边的 click 状态 为 true
-      graph.setItemState(edgeItem, "click", true);
+      if (e.target.get("name") === "collapse-icon") {
+        e.item.getModel().collapsed = !e.item.getModel().collapsed;
+        graph.setItemState(e.item, "collapsed", e.item.getModel().collapsed);
+        graph.layout();
+      }
     });
   });
 </script>
 
-<div class=" font-thin bg-red-50 text-center">test demo</div>
-<div id="graph-container" class=" container mx-auto bg-blue-100 px-4 py-4" />
+<div class="grid-bg full-screen">
+  <!-- navbar -->
+  <div class="mx-4">
+    <div class="navbar bg-base-100 top-4 shadow-xl shadow-blue-200 rounded-md">
+      <!-- 最左边 -->
+      <div class="navbar-start">
+        <a class="btn btn-ghost normal-case text-xl">English KG</a>
+      </div>
+      <!-- 中间搜索 -->
+      <div class="navbar-center">
+        <div class="form-control">
+          <div class="input-group">
+            <input
+              type="text"
+              placeholder="输入你要查询的单词..."
+              class="input input-bordered bg-base-200"
+            />
+            <button class="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                ><path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                /></svg
+              >
+            </button>
+          </div>
+        </div>
+      </div>
+      <!-- 最右边 -->
+      <div class="navbar-end">
+        <ul class="menu menu-horizontal px-1">
+          <li><a>网站介绍</a></li>
+          <li><a>关于我</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <!-- graph-container -->
+  <div class="bg-base-50 full-screen">
+    <div id="graph-container" />
+  </div>
+</div>
